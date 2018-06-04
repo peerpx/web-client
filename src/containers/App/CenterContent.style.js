0d9b80061f9bd5a737929684af34f20e61c606ae
Benjamin Mosnier <bm@kappuccino.org>
2018-06-04 22:20:20 +0200

    New PhotoCard component

---
+++ src/containers/App/CenterContent.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Centered = styled.div`
+	min-height: calc(100vh - 50px);
+	overflow-y: scroll;
+`
+Centered.Content = styled.div`
+	width: 900px;
+	margin: auto;
+	
+	.fullWidth{
+		width: 100%;
+	}
+`
+
+export default Centered
---
+++ src/containers/App/PhotoCard.js
@@ -0,0 +1,63 @@
+import React,{Component} from 'react'
+import styled from 'styled-components'
+
+import PhotoThumbnail from '../../components/photo-thumbnail'
+
+const Wrapper = styled.div`
+	margin-bottom: 20px;
+	position: relative;
+
+	.progress{
+		position: absolute;
+		top: 0; right: 0; left: 0;
+		transition: all 350ms;
+		
+		&.completed{
+			width: 100%;
+			opacity: 0;
+		}
+		
+		.bar{
+			height: 3px;
+			background: #ff7c8c;
+			transition: all 350ms;
+		}
+	}
+	
+	.media{
+		img{
+			width: 100%;
+			height: auto;
+		}
+	}
+`
+
+class PhotoCard extends Component {
+
+	render() {
+
+		const photo = this.props.photo
+
+		return (
+			<Wrapper>
+				<pre>{JSON.stringify(this.props.photo, null, 2)}</pre>
+
+				{photo.progress &&
+				<div className={`progress ${photo.progress === 100 ? 'completed' : ''}`}>
+					<div className="bar" style={{width: photo.progress+'%'}}/>
+				</div>
+				}
+
+				<div className="media">
+					{photo.preview && <img src={photo.preview} /> }
+					{!photo.preview && <PhotoThumbnail photo={this.props.photo} width={900} />}
+				</div>
+
+
+
+			</Wrapper>
+		)
+	}
+}
+
+export default PhotoCard
